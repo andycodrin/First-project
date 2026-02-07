dd862fb7799628076a2728d686f3a9ecd9b07268
Andy Cristea <andy.codrin2@gmail.com>
2026-02-07 21:10:17 +0200

    Test on branch

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
 print("Primul meu proiect pe GitHub!")
 print("Second version of the project")
+
+num = input("Enter a number: ")
+if num.isdigit():
+    if (int(num) % 2 == 0):
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+else:
+    print("Invalid input. Please enter a valid number.")    
